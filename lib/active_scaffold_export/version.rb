b9a80fa7432fc99eddf2ca9d5e648440c97fd0d0
Sergio Cambra <sergio@enpijama.es>
2024-04-16 16:06:02 +0200

    bump to 3.6.0

--- lib/active_scaffold_export/version.rb
+++ lib/active_scaffold_export/version.rb
@@ -1,8 +1,8 @@
 module ActiveScaffoldExport
   module Version
     MAJOR = 3
-    MINOR = 5
-    PATCH = 1
+    MINOR = 6
+    PATCH = 0
 
     STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
