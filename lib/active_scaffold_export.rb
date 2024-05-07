941a4339a63ca2f1ca9e15a0c85a5e70516bc790
Sergio Cambra <sergio@enpijama.es>
2024-05-07 13:25:53 +0200

    Revert "use ActiveScaffold hook on initialization"
    This reverts commit 796c416a780a740f9fe5b73bf20164177aecf6bd.

--- lib/active_scaffold_export.rb
+++ lib/active_scaffold_export.rb
@@ -1,3 +1,6 @@
+ACTIVE_SCAFFOLD_EXPORT_GEM = true
+ActiveScaffold rescue throw "should have included ActiveScaffold plug in first.  Please make sure that this plug-in comes alphabetically after the ActiveScaffold plug-in"
+
 require 'active_scaffold_export/engine'
 require 'active_scaffold_export/version'
 
@@ -6,3 +9,19 @@ module ActiveScaffoldExport
     File.dirname(__FILE__) + "/.."
   end
 end
+
+module ActiveScaffold
+  module Actions
+    ActiveScaffold.autoload_subdir('actions', self, File.dirname(__FILE__))
+  end
+
+  module Config
+    ActiveScaffold.autoload_subdir('config', self, File.dirname(__FILE__))
+  end
+
+  module Helpers
+    ActiveScaffold.autoload_subdir('helpers', self, File.dirname(__FILE__))
+  end
+end
+
+ActionView::Base.send(:include, ActiveScaffold::Helpers::ExportHelpers)
