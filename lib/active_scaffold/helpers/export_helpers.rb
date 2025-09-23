5350e38545032fd7b3b1ff58c4899fb80a7adf86
Sergio Cambra <sergio@enpijama.es>
2025-09-23 19:41:23 +0200

    return export options with a helper, so they can be overrided, to change them or add new options

--- lib/active_scaffold/helpers/export_helpers.rb
+++ lib/active_scaffold/helpers/export_helpers.rb
@@ -94,6 +94,28 @@ module ActiveScaffold
         end
       end
 
+      def export_options
+        export_config = active_scaffold_config.export
+        rows = []
+        rows << [
+          [check_box_tag('skip_header', 1, export_config.default_skip_header), :omit_header],
+          [text_field_tag('delimiter', export_config.default_delimiter, :size => 1, :maxlength => 1), :delimiter]
+        ]
+        if export_config.allow_full_download
+          rows << [
+            [radio_button_tag('full_download', false, !export_config.default_full_download), :this_page],
+            [radio_button_tag('full_download', true, export_config.default_full_download), :all_pages]
+          ]
+        end
+        if export_config.formats.many?
+          rows << export_config.formats.map do |format|
+            [radio_button_tag('format', format, export_config.default_file_format.to_sym == format), format.upcase.to_s]
+          end
+        else
+          rows << [hidden_field_tag('format', export_config.formats[0])]
+        end
+      end
+
     end
   end
 end
